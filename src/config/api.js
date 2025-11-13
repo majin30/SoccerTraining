542a68e56b47da68dde86ce2e0f5cc458e97803d
majin30 <majin356@gmail.com>
2025-11-13 08:50:42 -0600

    fix: paquetes

--- src/config/api.js
+++ src/config/api.js
@@ -1,11 +1,7 @@
-import apiGenerated from './api-generated.json';
-
-const DEFAULT_IP = '172.29.0.161';
+const HOST = '172.29.0.161';
 const PORT = 5000;
 
-const currentIp = apiGenerated?.ip ?? DEFAULT_IP;
-
-export const API_URL = `http://${currentIp}:${PORT}/api`;
+export const API_URL = `http://${HOST}:${PORT}/api`;
 
 
 
